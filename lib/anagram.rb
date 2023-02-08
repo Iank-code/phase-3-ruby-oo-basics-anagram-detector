97993e0af2d1ed29b27143bf69926b426b4e7e45
ianK-code <kian99564@gmail.com>
2023-02-08 23:31:31 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+=begin
+create class Anagram and create method initialize.
+Make sure to have an instance variable of your choice
+create method match that accepts an array since it will have a parameter
+    loop through the parameter and return individual item
+        split the item using split('') and sort it
+        split the instance variable using split('') and sort it
+
+        use == to match the the two and return it
+
+=end
+
+class Anagram
+    attr_accessor :word
+    def initialize word
+      @word = word
+    end
+    def match item
+        item.select do |new_word|
+            new_word.split('').sort == @word.split('').sort
+        end
+    end
+end
